1b45f49514eaf847d20741c2cef967b015bab1de
fabliny-ab <fabliny.ab@gmail.com>
2024-01-02 21:53:48 -0300

    personalizando hero

---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+var typed = new typed(".text", {
+    Strings: ["Desenvolvedora de Software", "Desenvolvedora Web", "Desenvolvedora Full Stack"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
